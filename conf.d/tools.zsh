5641081376e75eb7b02e07caeebcf25ff28c7bb5
Jacob LeCoq <jacob.lecoq.ext@bayer.com>
2026-04-17 19:31:10 -0500

    chore: update zsh configuration to include local bin path
    - Add check to include "$HOME/.local/bin" in the PATH if the directory exists, enhancing the environment setup.

--- conf.d/tools.zsh
+++ conf.d/tools.zsh
@@ -1,4 +1,5 @@
 # Tool-specific PATH and environment hooks.
 
 [[ -d "$HOME/.opencode/bin" ]] && path=("$HOME/.opencode/bin" $path)
+[[ -d "$HOME/.local/bin" ]] && path=("$HOME/.local/bin" $path)
 [[ -r "$HOME/.local/bin/env" ]] && source "$HOME/.local/bin/env"
