b4be09bc02897018be8c37a9e1c1d845685e6da8
Jacob LeCoq <lecoqjacob@gmail.com>
2026-06-19 11:19:19 -0500

    Enhance macOS paths in zsh configuration: add /opt/homebrew/bin and /usr/local/bin to PATH for better access to manually-installed CLIs.

--- conf.d/macos-paths.zsh
+++ conf.d/macos-paths.zsh
@@ -11,3 +11,10 @@ path=(
   $HOMEBREW_PREFIX/share/npm/bin(N)
   $path
 )
+
+# /usr/local/bin for manually-installed CLIs (tailscale, etc.)
+path=(
+  /opt/homebrew/bin(N)
+  /usr/local/bin(N)
+  $path
+)
